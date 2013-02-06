122fe2de01b7800412ac35a9da3b30ede9dd2901
Cristobal Pizarro <cristobal@brium.cl>
2013-02-05 23:11:54 -0300

    v2

---
+++ js/googlemaps.js
@@ -0,0 +1,31 @@
+$(function(){
+
+$('#googleMap').gmap3({ 
+			action:'init',
+            options:{
+              center:[-33.454523,-70.616251],
+              zoom: 16,
+              scrollwheel: false,
+              streetViewControl: false,
+              disableDefaultUI: true,
+              styles:
+              [
+              	{featureType: "all", stylers: [{saturation: -100}]},
+              	{featureType: "all", stylers: [{invert_lightness: true}]},
+              ]
+            }
+          },
+          { action: 'addMarkers',
+            markers:[
+              {lat:-33.454523, lng:-70.616251, options:{icon:"http://www.brium.cl/white/images/pin.png"}},
+            ],
+            marker:{
+              options:{
+                draggable: false,
+              },
+              
+            }
+          }
+        );
+});
+    
